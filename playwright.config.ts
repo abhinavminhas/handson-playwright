48270e2c81b482787ddfaf1ff7e350c87b82c690
abhinavminhas <abhinavminhas@users.noreply.github.com>
2025-11-04 10:19:08 +1100

    config update - junit report

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['html', { open: 'never' }],
+    [ 'html', { open: 'never' } ],
+    [ 'junit' , { outputFile: 'playwright-report/results.xml' } ],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
