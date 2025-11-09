af0f7325c4b66c0eb1a0dd4063ff39302919de72
abhinavminhas <abhinavminhas@users.noreply.github.com>
2025-11-10 00:22:16 +1100

    config update

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     [ 'html', { open: 'never' } ],
-    [ 'junit' , { outputFile: 'playwright-report/results.xml' } ],
+    [ 'junit' , { outputFile: 'playwright-report/junit/results.xml' } ],
     [ 'list' ]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
